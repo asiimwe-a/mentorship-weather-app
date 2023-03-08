b84b27dcbf10ac36d862c75b72f5708ede002e55
daveclinton <clintondavid46@gmail.com>
2023-03-08 12:43:56 +0300

    Add weather app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-import Searchbar from './components/input';
+import * as React from "react";
+import WeatherChart from "./components/WeatherChart";
+import { Box } from "@chakra-ui/layout";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-     <Searchbar setSearch={function (value: React.SetStateAction<string | undefined>): void {
-        throw new Error('Function not implemented.');
-      } } setGeolocate={function (value: React.SetStateAction<boolean>): void {
-        throw new Error('Function not implemented.');
-      } } geolocate={false}/>
-    </div>
+    <Box bg="#f5fbff" h="100vh" p={4}>
+      <WeatherChart />
+    </Box>
   );
 };
 
---
+++ src/components/WeatherChart/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const WeatherChart: React.FC = () => {
+  return <div>WeatherChart</div>;
+};
+
+export default WeatherChart;
