34f3dab73019d5a147d4592b09718f3d25b2d946
Habibayvonne <habibayvonne198@gmail.com>
2023-03-08 10:14:42 +0300

    add input uI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Searchbar from "./components/input"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Searchbar setSearch={function (value: React.SetStateAction<string | undefined>): void {
+        throw new Error('Function not implemented.');
+      } } setGeolocate={function (value: React.SetStateAction<boolean>): void {
+        throw new Error('Function not implemented.');
+      } } geolocate={false}/>
+      
     </div>
   );
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { FaSearch, FaMapMarkerAlt } from "react-icons/fa";
+
+export default function Searchbar(search: {
+    setSearch: Dispatch<SetStateAction<string | undefined>>;
+    setGeolocate: Dispatch<SetStateAction<boolean>>;
+    geolocate: boolean;
+  }) {
+    const [input, setInput] = useState<string | undefined>();
+  
+    useEffect(() => {
+      const listener = (event: { key: string; preventDefault: () => void }) => {
+        //detects if the user is pressing the enter key to start a search
+        if (event.key === "Enter") {
+          event.preventDefault();
+          search.setSearch(input);
+        }
+      };
+      document.addEventListener("keydown", listener);
+      return () => {
+        document.removeEventListener("keydown", listener);
+      };
+    });
+
+    return (
+        <div className="flex  items-center">
+          <input
+            placeholder="search"
+            className="n"
+            onChange={(e) => setInput(e.target.value)}
+            onFocus={(e) => {
+              e.target.value = "";
+            }}
+          ></input>
+          <div
+            className="n"
+            data-tip="get data by user search"
+          >
+            <FaSearch
+              className="cursor-pointer"
+              onClick={() => search.setSearch(input)}
+            />
+          </div>
+          <div className="tooltip tooltip-bottom" data-tip="activate user geolocation">
+            <FaMapMarkerAlt
+              className="cursor-pointer"
+              onClick={() => search.setGeolocate(!search.geolocate)}
+            />
+          </div>
+        </div>
+      );
+    }
+
