cb0993fde1d34ff1f8c3cd6ea15b4c383da8b1b0
daveclinton <clintondavid46@gmail.com>
2023-03-10 23:47:49 +0300

    feat: Add weather chart

--- src/components/WeatherChart/index.tsx
+++ src/components/WeatherChart/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Box, Flex, Text, Heading } from "@chakra-ui/layout";
 import Searchbar from "../InputField/input";
 import LineChart from "./LineChart";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 
 const WeatherChart: React.FC = () => {
   return (
@@ -28,20 +29,36 @@ const WeatherChart: React.FC = () => {
           <Heading fontSize="24px" fontWeight={500}>
             New York, US
           </Heading>
-          <Flex mt="24px">
+          <Flex mt="40px">
             <Heading fontSize="72px" fontWeight={500}>
               84
             </Heading>
             <Text fontSize="24px">&#8457;</Text>
           </Flex>
           <Text>Feels 79 &#8457; </Text>
-          <Text mt="48px" fontSize="34px">
+          <Text mt="60px" fontSize="34px">
             Partly Cloudy
           </Text>
         </Box>
-        <Box w="90%" h="300px">
-          <LineChart />
-        </Box>
+
+        <Tabs variant="soft-rounded">
+          <TabList gap="30px" pl="170px">
+            <Tab>Temperature</Tab>
+            <Tab>Precipitation</Tab>
+            <Tab>Wind</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <LineChart />
+            </TabPanel>
+            <TabPanel>
+              <LineChart />
+            </TabPanel>
+            <TabPanel>
+              <LineChart />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Flex>
     </Flex>
   );
