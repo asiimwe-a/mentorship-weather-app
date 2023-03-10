357bbf3e69819ee981ffdb79dee1b23015f165af
daveclinton <clintondavid46@gmail.com>
2023-03-10 22:42:24 +0300

    feat: Add chart design responsiveness

--- src/components/WeatherChart/index.tsx
+++ src/components/WeatherChart/index.tsx
@@ -5,7 +5,13 @@ import LineChart from "./LineChart";
 
 const WeatherChart: React.FC = () => {
   return (
-    <Flex w="70%" bg="#ffffff" m="48px 0px 0px 24px" flexDir="column" p="30px">
+    <Flex
+      maxW="60%"
+      bg="#ffffff"
+      m="48px 0px 0px 24px"
+      flexDir="column"
+      p="30px"
+    >
       <Searchbar
         setSearch={function (
           value: React.SetStateAction<string | undefined>
@@ -13,8 +19,12 @@ const WeatherChart: React.FC = () => {
           throw new Error("Function not implemented.");
         }}
       />
-      <Flex justifyContent="space-between" mt="72px">
-        <Box>
+      <Flex
+        justifyContent="space-between"
+        gap={{ base: "0", xl: "6%" }}
+        mt="72px"
+      >
+        <Box minW={{ xl: "40%" }}>
           <Heading fontSize="24px" fontWeight={500}>
             New York, US
           </Heading>
@@ -25,9 +35,11 @@ const WeatherChart: React.FC = () => {
             <Text fontSize="24px">&#8457;</Text>
           </Flex>
           <Text>Feels 79 &#8457; </Text>
-          <Text fontSize="34px">Partly Cloudy</Text>
+          <Text mt="48px" fontSize="34px">
+            Partly Cloudy
+          </Text>
         </Box>
-        <Box w="700px" h="400px">
+        <Box w="90%" h="300px">
           <LineChart />
         </Box>
       </Flex>
