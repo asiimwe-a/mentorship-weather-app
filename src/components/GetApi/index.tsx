5c59052d5e47419c9bceea0d30b4205d6982cb20
gitahievans <gitahievance@gmail.com>
2023-03-12 06:25:34 +0300

    Change component name and add dependencies to useEffect

--- src/components/GetApi/index.tsx
+++ src/components/GetApi/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const GetData: React.FC = (city) => {
-
   useEffect(() => {
     weatherData();
   }, [city]);
@@ -11,8 +10,8 @@ const GetData: React.FC = (city) => {
     try {
       let response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5a6a3604d50f420072819316b2ed23a2`
-      )
-      console.log(response)
+      );
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
